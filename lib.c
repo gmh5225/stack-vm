d1903cb0c937d7738dd520e4b973c45d2cc84851
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-09-16 05:18:45 +0100

    (lib)+dynamic arrays,+buffers now have a "name"
    Dynamic homogeneous arrays which reallocate themselves.  Useful for a
    variety of purposes.
    
    Text buffers now have a name property for their source (filepath,
    repl, etc)

--- lib.c
+++ lib.c
@@ -7,9 +7,10 @@
 #include "./lib.h"
 
 #include <ctype.h>
+#include <malloc.h>
 #include <string.h>
 
-buffer_t buffer_read_file(FILE *fp)
+buffer_t buffer_read_file(const char *name, FILE *fp)
 {
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
@@ -18,6 +19,7 @@ buffer_t buffer_read_file(FILE *fp)
   buffer_t buffer  = {0};
   size_t data_size = sizeof(buffer.data[0]);
 
+  buffer.name      = name;
   buffer.data      = calloc(size, data_size);
   buffer.available = size;
   buffer.cur       = 0;
@@ -27,10 +29,11 @@ buffer_t buffer_read_file(FILE *fp)
   return buffer;
 }
 
-buffer_t buffer_read_cstr(const char *str, size_t size)
+buffer_t buffer_read_cstr(const char *name, const char *str, size_t size)
 {
   buffer_t buffer  = {0};
   size_t data_size = sizeof(buffer.data[0]);
+  buffer.name      = name;
   buffer.data      = calloc(size, data_size);
   buffer.available = size;
   buffer.cur       = 0;
@@ -53,10 +56,37 @@ void buffer_seek_next(buffer_t *buffer)
 
 bool buffer_is_end(buffer_t buf)
 {
-  return buf.cur >= buf.available;
+  return buf.cur >= buf.available || buf.cur == buf.available - 1;
 }
 
 size_t buffer_space_left(buffer_t buf)
 {
   return buf.available - buf.cur;
+}
+
+void darr_init(darr_t *darr, size_t initial_size, size_t member_size)
+{
+  darr->member_size = member_size;
+  darr->used        = 0;
+  darr->available   = initial_size;
+  darr->data        = calloc(initial_size, member_size);
+}
+
+void darr_free(darr_t *darr)
+{
+  free(darr->data);
+}
+
+void darr_ensure_capacity(darr_t *darr, size_t wanted)
+{
+  if (darr->used + wanted >= darr->available)
+    darr->data = reallocarray(
+        darr->data,
+        MAX(darr->used + wanted, darr->available * DARR_REALLOC_MULT),
+        darr->member_size);
+}
+
+void darr_tighten(darr_t *darr)
+{
+  darr->data = reallocarray(darr->data, darr->used, darr->member_size);
 }
--- lib.h
+++ lib.h
@@ -7,24 +7,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DEBUG 0
+#define DEBUG     0
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) > (b) ? (b) : (a))
 
 typedef uint64_t u64;
 typedef int64_t i64;
 
 typedef u64 word;
 
+/* Expandable text buffers */
 typedef struct
 {
+  const char *name;
   char *data;
   size_t cur, available;
 } buffer_t;
 
-buffer_t buffer_read_file(FILE *fp);
-buffer_t buffer_read_cstr(const char *, size_t);
+buffer_t buffer_read_file(const char *, FILE *fp);
+buffer_t buffer_read_cstr(const char *, const char *str, size_t);
 char buffer_peek(buffer_t);
 void buffer_seek_next(buffer_t *);
 bool buffer_is_end(buffer_t);
 size_t buffer_space_left(buffer_t);
+
+/* Expandable generic buffers */
+#define DARR_REALLOC_MULT 1.5
+#define DARR_INITAL_SIZE  64
+typedef struct
+{
+  void *data;
+  size_t member_size, used, available;
+} darr_t;
+
+void darr_init(darr_t *, size_t initial_size, size_t member_size);
+void darr_free(darr_t *);
+void darr_ensure_capacity(darr_t *, size_t);
+void darr_tighten(darr_t *);
+
+#define DARR_APP(DARR, TYPE, OBJ)                 \
+  do                                              \
+  {                                               \
+    darr_ensure_capacity(DARR, 1);                \
+    (((TYPE)(DARR)->data)[(DARR)->used++] = OBJ); \
+  } while (0);
 
 #endif
