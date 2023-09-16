3b7f80dca92507e26f5cdca07300aa77ab39b187
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-09-16 18:03:31 +0100

    (tests/test-lib)+implemented test_lib_buffer_read_file
    Creates a random filename (up to 10 unique names), writes some mock
    data, then opens it again to try and slurp with buffer_read_file.
    Tests if that works.

--- tests/test-lib.c
+++ tests/test-lib.c
@@ -9,6 +9,9 @@
 
 #include "../lib.h"
 
+#include <errno.h>
+#include <string.h>
+
 bool test_lib_MAX(void)
 {
   ASSERT(test_1, MAX(0, 1) == 1);
@@ -32,5 +35,47 @@ bool test_lib_ARR_SIZE(void)
   int64_t test_ints[test_ints_size];
   printf("\t\t[INFO]: test_lib_ARR_SIZE: test_ints=%lu\n", test_ints_size);
   ASSERT(test_2, ARR_SIZE(test_ints) == test_ints_size);
+  return test_1 && test_2;
+}
+
+bool test_lib_buffer_read_file(void)
+{
+  // Create a mock file (using a random number to ensure we're not
+  // conflicting with something in the workspace
+  const char text[] = "This is some text, I hope buffer will slurp it all!\n"
+                      "Here is another line\n"
+                      "And my final line\n";
+
+  size_t n = rand() % 10;
+  char filepath[30];
+  sprintf(filepath, "tests/TEST_LIB_MOCK_FILE%lu.txt", n);
+  FILE *fp = fopen(filepath, "w");
+  if (!fp)
+  {
+    printf("\t\t[INFO]: test_lib_buffer_read_file: Could not open file (to "
+           "write) `%s`: "
+           "%s\n",
+           filepath, strerror(errno));
+    return false;
+  }
+  fwrite(text, 1, ARR_SIZE(text), fp);
+  fclose(fp);
+
+  fp = fopen(filepath, "r");
+  if (!fp)
+  {
+    printf("\t\t[INFO]: test_lib_buffer_read_file: Could not open file (to "
+           "read) `%s`: "
+           "%s\n",
+           filepath, strerror(errno));
+    return false;
+  }
+  buffer_t buf = buffer_read_file(filepath, fp);
+  fclose(fp);
+
+  ASSERT(test_1, buf.available == ARR_SIZE(text));
+  ASSERT(test_2, strncmp(buf.data, text, ARR_SIZE(text)) == 0);
+
+  free(buf.data);
   return test_1 && test_2;
 }
