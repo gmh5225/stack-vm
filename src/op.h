971ee79a196869560221a7ae31db0b507d57660e
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-09-19 16:23:16 +0100

    (op)~Fixed an issue with creating instructions with no operands
    If I didn't specify the operand, it can be *any* value.  Better to 0

--- src/op.h
+++ src/op.h
@@ -18,18 +18,17 @@ typedef struct
     OP_LABEL,
     OP_JUMP_REL,
     OP_JUMP_LABEL,
-
     NUMBER_OF_OPERATORS,
   } opcode;
 
   i64 operand;
 } op_t;
 
-#define OP_CREATE_HALT       ((op_t){.opcode = OP_HALT})
+#define OP_CREATE_HALT       ((op_t){.opcode = OP_HALT, .operand = 0})
 #define OP_CREATE_PUSH(x)    ((op_t){.opcode = OP_PUSH, .operand = x})
-#define OP_CREATE_PLUS       ((op_t){.opcode = OP_PLUS})
+#define OP_CREATE_PLUS       ((op_t){.opcode = OP_PLUS, .operand = 0})
 #define OP_CREATE_DUP(x)     ((op_t){.opcode = OP_DUP, .operand = x})
-#define OP_CREATE_PRINT      ((op_t){.opcode = OP_PRINT})
+#define OP_CREATE_PRINT      ((op_t){.opcode = OP_PRINT, .operand = 0})
 #define OP_CREATE_JMP_REL(x) ((op_t){.opcode = OP_JUMP_REL, .operand = x})
 #define OP_CREATE_JMP_LBL(x) ((op_t){.opcode = OP_JUMP_LABEL, .operand = x})
 #define OP_CREATE_LABEL(x)   ((op_t){.opcode = OP_LABEL, .operand = x})
