1638ef5f3cff056c190c14e03e506ef2e95be3a4
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-09-22 00:17:24 +0100

    Added a .dir-locals file for Emacs
    Just adds some commands for projectile, helpful for keybindings.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((projectile-project-compilation-cmd . "make")
+         (projectile-project-run-cmd         . "make run")
+         (projectile-project-test-cmd        . "make test")
+         (projectile-project-configure-cmd   . "make clean"))))
