1cfec37f0237257c3e26006a0a1e11958adb1716
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-09-26 15:33:46 +0100

    (tests/test-lexer)+recipe for testing comment tokenisation

--- tests/test-lexer.h
+++ tests/test-lexer.h
@@ -8,6 +8,7 @@ bool test_tokenise_whitespace(void);
 bool test_tokenise_symbol(void);
 bool test_tokenise_character(void);
 bool test_tokenise_number_literal(void);
+bool test_tokenise_comments(void);
 bool test_tokenise_errors(void);
 
 static const test_t TEST_LEXER_SUITE[] = {
@@ -15,6 +16,7 @@ static const test_t TEST_LEXER_SUITE[] = {
     CREATE_TEST(test_tokenise_whitespace), CREATE_TEST(test_tokenise_symbol),
     CREATE_TEST(test_tokenise_character),
     /* CREATE_TEST(test_tokenise_number_literal), */
+    /* CREATE_TEST(test_tokenise_comments), */
     /* CREATE_TEST(test_tokenise_errors), */
 };
 #endif
