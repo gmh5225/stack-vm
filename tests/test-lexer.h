18064c84cba7afca31a8bb4e27d71d3b7c3676b3
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-09-26 16:33:37 +0100

    (tests/test-lexer)~tokenise_number_literal->tokenise_number

--- tests/test-lexer.h
+++ tests/test-lexer.h
@@ -7,15 +7,16 @@ bool test_tokenise_one_character(void);
 bool test_tokenise_whitespace(void);
 bool test_tokenise_symbol(void);
 bool test_tokenise_character(void);
-bool test_tokenise_number_literal(void);
+bool test_tokenise_number(void);
 bool test_tokenise_comments(void);
 bool test_tokenise_errors(void);
 
 static const test_t TEST_LEXER_SUITE[] = {
     CREATE_TEST(test_tokenise_one_character),
-    CREATE_TEST(test_tokenise_whitespace), CREATE_TEST(test_tokenise_symbol),
+    CREATE_TEST(test_tokenise_whitespace),
+    CREATE_TEST(test_tokenise_symbol),
     CREATE_TEST(test_tokenise_character),
-    /* CREATE_TEST(test_tokenise_number_literal), */
+    CREATE_TEST(test_tokenise_number),
     /* CREATE_TEST(test_tokenise_comments), */
     /* CREATE_TEST(test_tokenise_errors), */
 };
